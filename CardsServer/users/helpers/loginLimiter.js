cee46bfce302e3b3977a1a048a430c6c624c2dc0
reutrose <reutroz@gmail.com>
2025-01-23 10:32:56 +0200

    fixing login-limiter issues

--- CardsServer/users/helpers/loginLimiter.js
+++ CardsServer/users/helpers/loginLimiter.js
@@ -2,8 +2,8 @@ const rateLimit = require("express-rate-limit");
 const { handleError } = require("../../utils/handleErrors");
 
 const loginLimiter = rateLimit({
-	windowMs: 24 * 60 * 60 * 1000,
-	max: 3,
+	windowMs: 86400000,
+	limit: 3,
 	message: "Too many login attempts. Try again after 24 hours.",
 	keyGenerator: (req, res) => {
 		return req.body.email || "unknown";
